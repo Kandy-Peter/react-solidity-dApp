6bbe15589e4a21f7507bffcc4ee51eab694fa079
kandy <kandypeter03@gmail.com>
2023-02-22 13:44:56 +0200

    Create projects components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
+import { Navbar, Transactions, Services, Welcome, Footer } from './components'
 
 function App() {
 
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <div className="min-h-screen">
+      <div className='gradient-bg-welcome'>
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div >
   )
 }
 
