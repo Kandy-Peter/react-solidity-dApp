c7a559a697af1319280789026f9630a0d6f625a6
kandy <kandypeter03@gmail.com>
2023-02-22 17:39:14 +0200

    Finalize smart_contract and deploy using hardhat

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
+require ('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.0",
+  networks: {
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/P0yIeAwKx6mc6tIp8CpwKYflJcrmOE3O",
+      accounts: [`${process.env.PRIVATE_KEY}`]
+    }
+  }
 };
