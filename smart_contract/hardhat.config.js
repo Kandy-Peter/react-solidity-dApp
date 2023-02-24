444b01a16bff674bedda2768f00d19f1ab5e5e6a
kandy <kandypeter03@gmail.com>
2023-02-24 20:15:08 +0200

    Change the alchemy url

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   solidity: "0.8.0",
   networks: {
     goerli: {
-      url: "https://eth-goerli.g.alchemy.com/v2/P0yIeAwKx6mc6tIp8CpwKYflJcrmOE3O",
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
       accounts: [`${process.env.PRIVATE_KEY}`]
     }
   }
